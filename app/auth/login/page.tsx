e7a1db0cffe2dadd545f6efc9ab4d5f7638578c6
Edward Wong <edward@meetgrit.com>
2024-11-03 16:57:50 -0500

    fix loginform

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,5 @@
 import LoginForm from "@/app/components/shadcn/block/authentication-02"
 
-export const description = "A simple login form."
-
-export const iframeHeight = "870px"
-
-export const containerClassName = "w-full h-full"
-
 export default function Page() {
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
