0a9755b30355bda199ed5a6c0082c7f1d2c76eae
Edward Wong <edward@meetgrit.com>
2024-11-03 21:26:41 -0500

    add environment in tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+import axios from "axios";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
@@ -16,6 +17,7 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8000';
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -24,7 +26,18 @@ export default function App() {
   }
 
   useEffect(() => {
-    listTodos();
+    axios.get(`${baseUrl}/auth/is-authenticated/`, { withCredentials: true })
+    .then((response) => {
+      console.log(response);
+      if (response.data.is_authenticated === true) {
+        listTodos();
+      } else {
+        window.location.href = `${baseUrl}/auth/login`;
+      }
+    })
+    .catch(() => {
+      window.location.href = `${baseUrl}/auth/login`;
+    });
   }, []);
 
   function createTodo() {
